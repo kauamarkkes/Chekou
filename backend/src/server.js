273cc5399b6d1de4b35dd5f6a9f8e3bca5a201c7
joao erick <gabrielbarbosamoura001@gmail.com>
2026-09-19 21:21:53 -0300

    estrutura do backend + rotas

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,15 @@
 
 const express = require('express'); //variavel com a lib express
 const app = express(); //servidor na variavel app
+const cors = require("cors"); //lib do cors (necessario para falar com o front-end)
 const PORTA = 3000;
-const user = require("./rotas/rotasUser"); //arquivo da rotas de usuario
+app.use(cors()); //permissao para outra porta conversar com o servidor
+const user = require("./rotas/loguin"); //arquivo da rotas de usuario
 const taref = require("./rotas/tarefas"); //arquivo da rota de tarefas
 app.use(express.json()); //ler tudo em formato json
  //o que acontecer mandar para rotasTaref
 
-app.use("/usuario", user); //o que acontecer na url usuarios, mandar para a variavel user
+app.use("/login", user); //o que acontecer na url usuarios, mandar para a variavel user
 app.use("/tarefas", taref); //o que acontecer em tarefas, mandar para taref
 
 app.get("/", (req, res) => {res.send("servidor rodando");}); // requisição pagina inicial
